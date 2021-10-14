05626c740285cb219e6f276355f234723f9b010a
Amie Foster <amie.n.foster@gmail.com>
2021-10-14 13:33:13 -0600

    added notes

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,18 +4,21 @@ class Triangle
 
   def initialize side_a, side_b, side_c
     
+    #condition to raise error - no side can be 0 there must be 3 sides with a value
     if side_a <= 0 || side_b <= 0 || side_c <= 0
       raise TriangleError
     end
-
+  #condition to raise error - the third side cannot be larger than the sum of the other  
+    #two sides
     if side_a >= (side_b + side_c) || side_b >= (side_a + side_c) || side_c >= (side_a + side_b)
       raise TriangleError
     end
-
+  #condition to raise error - all 3 sides have to exist
     if side_a == nil || side_b == nil || side_c == nil
       raise TriangleError
     end
-
+ 
+    #sets the value upon initialization of a new triangle instance
     @side_a = side_a
     @side_b = side_b
     @side_c = side_c
@@ -23,6 +26,7 @@ class Triangle
 
   end
 
+  #this method tells you what kind of triangle it is based on the relationship between sides
   def kind
     if @side_a == @side_b && @side_a == @side_c
       :equilateral
@@ -33,6 +37,7 @@ class Triangle
     end
   end
 
+  #this is the error to be raised if the sides are not acceptable
   class TriangleError < StandardError
     def message
       "This is not a valid triangle."
