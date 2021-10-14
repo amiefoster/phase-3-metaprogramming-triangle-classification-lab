b60d9047de9d36fffc1a2ffe93982c78149a46ad
Amie Foster <amie.n.foster@gmail.com>
2021-10-13 23:33:07 -0600

    3/6 deliverables

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
   # write code here
+  
+
+  def initialize side_a, side_b, side_c
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if @side_a == @side_b && @side_a == @side_c
+      :equilateral
+    elsif @side_a == @side_b || @side_b == @side_c || @side_c == @side_a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
 end
+
